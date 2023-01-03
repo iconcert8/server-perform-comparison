29a4cb58c2a64f9914a0d76f9e14d921fd87cd48
iconcert8 <51566869+iconcert8@users.noreply.github.com>
2023-01-03 19:54:30 +0900

    FEATURE: add create method to controller

--- apache-monolithic/src/main/java/com/gyeom/apachemonolithic/AppController.java
+++ apache-monolithic/src/main/java/com/gyeom/apachemonolithic/AppController.java
@@ -3,12 +3,22 @@ package com.gyeom.apachemonolithic;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class AppController {
+
+    private final UserRepository userRepository;
+
+    public AppController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/")
     public ResponseEntity<Object> root(){
@@ -16,5 +26,18 @@ public class AppController {
             put("status", 200);
         }};
         return ResponseEntity.ok().body(map);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> create(@RequestBody User user){
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping("/read/{id}")
+    public ResponseEntity<Object> read(@PathVariable("id") long id){
+        Optional<User> nullableUser = userRepository.findById(id);
+        if(nullableUser.isEmpty()) return ResponseEntity.notFound().build();
+        else return ResponseEntity.ok().body(nullableUser.get());
     }
 }
